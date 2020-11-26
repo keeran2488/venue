2242a704f347a8bd5bd812f17aa6c8674d326eca
Kiran Chaudhary <chaudharyk456@gmail.com>
2020-11-26 16:22:08 +0545

    created database

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import VenueDetails, Customer
+
+
+admin.site.register(Customer)
+admin.site.register(VenueDetails)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class VenueDetails(models.Model):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    details = models.TextField(max_length=255)
+    location = models.CharField(max_length=255)
+    phone = models.CharField(max_length=10)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Customer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(max_length=10)
+    profile_picture = models.ImageField(upload_to = 'profile/')
